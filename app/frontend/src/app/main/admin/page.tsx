1ea7ae983e0a0253988dd44fa867b053e3bd43ad
hsh980305 <hsh9835@gmail.com>
2024-02-09 20:13:55 +0900

    Main Test

---
+++ app/frontend/src/app/main/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+
+    return(
+        <h1>개발중</h1>
+    )
+
+}
---
+++ app/frontend/src/app/main/board/SearchResultType.tsx
@@ -0,0 +1,34 @@
+
+import {SearchProps} from "@/app/main/board/page";
+
+export function SearchResultType(props:SearchProps) {
+
+    props.search
+
+    const typeList:string[] = [
+        "React",
+        "NextJS",
+        "Spring Boot",
+        "MariaDB"
+    ]
+
+    return (
+        <>
+            <nav className={'flex'}>
+                <a className={'mx-2 ml-5'}>
+                    전체
+                </a>
+                <div className={'mx-2'}>/</div>
+                {
+                    typeList.map((type) => {
+                        return (
+                            <>
+                                <a className={'mx-2 hover:underline'}>{type}</a>
+                            </>
+                        )
+                    })
+                }
+            </nav>
+        </>
+    )
+}
