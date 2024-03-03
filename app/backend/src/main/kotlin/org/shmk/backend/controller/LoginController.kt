720ce6bf44616976a3bd6f53c70edfec3231f0e1
mikyeong-Jeong <jlj2427@naver.com>
2024-03-03 20:42:46 +0900

    20240303 작업분

--- app/backend/src/main/kotlin/org/shmk/backend/controller/LoginController.kt
+++ app/backend/src/main/kotlin/org/shmk/backend/controller/LoginController.kt
@@ -2,12 +2,12 @@ package org.shmk.backend.controller
 
 import org.shmk.backend.dto.req.LoginReq
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class LoginController {
     @ResponseBody
     @PostMapping("/login", consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
