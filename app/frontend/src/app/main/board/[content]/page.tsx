3d94ab3f466e2eb886d0c0b490926b68d982cf1e
hsh980305 <hsh9835@gmail.com>
2024-03-17 17:37:14 +0900

    Viwer Edidtor 구현

--- app/frontend/src/app/main/board/[content]/page.tsx
+++ app/frontend/src/app/main/board/[content]/page.tsx
@@ -1,9 +1,11 @@
 import {MKServer} from "@/api/MKServer";
+import ToastUIEditor from "@components/toast_ui/editor/toast_ui_editor";
+import ToastUIViewer from "@components/toast_ui/editor/toast_ui_viewer";
 
 export default async function Content(props:any) {
 
     const boardContent = MKServer.get('/hello')
-        .then(e=>e.data)
+        .then(e=> e.data)
         .catch(() => 'error')
 
     return (
@@ -12,6 +14,7 @@ export default async function Content(props:any) {
                 {props.params.content}
                 {boardContent}
             </div>
+            <ToastUIViewer/>
         </>
     );
 }
