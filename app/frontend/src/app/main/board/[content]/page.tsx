3b72a37fb6b626e73c083084d0c829295dc923ac
hsh980305 <hsh9835@gmail.com>
2024-04-05 15:48:33 +0900

    oAuth 작업

--- app/frontend/src/app/main/board/[content]/page.tsx
+++ app/frontend/src/app/main/board/[content]/page.tsx
@@ -1,19 +1,19 @@
-import {MKServer} from "@/api/MKServer";
+import {MKServer} from "@/pages/api/MKServer";
 import ToastUIEditor from "@components/toast_ui/editor/toast_ui_editor";
 import ToastUIViewer from "@components/toast_ui/editor/toast_ui_viewer";
 
 export default async function Content(props:any) {
 
-    const boardContent = MKServer.get('/hello')
+    const boardContent:Promise<string> = await MKServer.get('/hello')
         .then(e=> e.data)
         .catch(() => 'error')
 
+    console.log(boardContent)
+    console.log(typeof boardContent)
+
     return (
         <>
-            <div>
-                {boardContent}
-            </div>
-            <ToastUIViewer/>
+            <ToastUIViewer content={"# Hello World"}/>
         </>
     );
 }
