62a9436fcdd486bbb68130a5c9b3b6a23a37199e
hsh980305 <hsh9835@gmail.com>
2024-05-05 23:06:13 +0900

    Swagger 설정 완료

--- app/backend/build.gradle.kts
+++ app/backend/build.gradle.kts
@@ -31,11 +31,14 @@ val snippetsDir by extra { file("build/generated-snippets") }   // 변경
 extra["springCloudAzureVersion"] = "5.9.1"
 
 dependencies {
-    // swagger ui 추가
-    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    // swagger ui 추가 springfox 지원 중단한지 오래되어서 spring docs로 교체
+//    implementation("io.springfox:springfox-boot-starter:3.0.0")
     implementation("io.springfox:springfox-swagger-ui:3.0.0")
+//    implementation("org.springdoc:springdoc-openapi-webmvc-core:1.8.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
@@ -55,17 +58,13 @@ dependencies {
     implementation("org.springframework.data:spring-data-rest-hal-explorer")
 
     // 테스트 위해서 추가
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf") // Thymeleaf 의존성
+//    implementation("org.springframework.boot:spring-boot-starter-thymeleaf") // Thymeleaf 의존성
 
-    implementation("org.springframework:spring-jdbc")
+//    implementation("org.springframework.:spring--jdbc")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
-    implementation("org.springframework.session:spring-session-jdbc")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
@@ -73,6 +72,9 @@ dependencies {
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
+
+    // gson
+    implementation ("com.google.code.gson:gson:2.8.6")
 
     // jwt 설정
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
--- app/backend/src/main/kotlin/org/shmk/backend/config/SwaggerConfig.kt
+++ app/backend/src/main/kotlin/org/shmk/backend/config/SwaggerConfig.kt
@@ -1,24 +1,23 @@
 package org.shmk.backend.config
 
-import io.swagger.models.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
-import springfox.documentation.builders.PathSelectors
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
+import org.springframework.context.annotation.Configuration
 
-//@Configuration
+@Configuration
 class SwaggerConfig {
 
-    @Bean
-    fun api(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.any())
-//            .paths(PathSelectors.any())
-            .paths(PathSelectors.ant("/**/api/**"))
-            .build()
-    }
+//    @Bean
+//    fun api(): Docket {
+//        return Docket(DocumentationType.SWAGGER_2)
+//            .select()
+//            .apis(RequestHandlerSelectors.any())
+////            .paths(PathSelectors.any())
+//            .paths(PathSelectors.ant("/**/api/**"))
+//            .build()
+//    }
     /*
     @Bean
     fun openAPI(): OpenAPI {
@@ -37,7 +36,14 @@ class SwaggerConfig {
             .components(components)
     }*/
 
-    private fun apiInfo(): Info {
+    @Bean
+    public fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .components(Components())
+            .info(apiInfo())
+    }
+
+    private fun apiInfo(): Info? {
         return Info()
             .title("API Test") // API의 제목
             .description("Let's practice Swagger UI") // API에 대한 설명
